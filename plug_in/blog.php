414b3391a42b75e4845fb275997afa52a31ddc6b
pundrik mishra <pundrikmishra1234@gmail.com>
2019-07-13 18:00:36 +0530

    commit
     retriveve data form database and display on the web

--- plug_in/blog.php
+++ plug_in/blog.php
@@ -1,31 +1,30 @@
 <?php
-    $data= array();
+    //$data= array();
     $select= $conn->prepare("select * from `blog` order by `id` desc");
     $select->execute();
-    
-    
-    foreach($result as $row){
-            $data[]=$row;
-        
-    }
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
+    $result = $select->fetchAll(\PDO::FETCH_ASSOC);
+    // foreach($result as $row)
+    //     {
+    //        $data[]=$row;
+    //     }
+    // echo "<pre>";
+    // print_r($result);
+    // echo "</pre>";
 ?>
 <h2> Blog Updates</h2>
 <div class="container">
     <?php
-    for($i=0; $i<10; $i++){
+    for($i=0; $i<count($result); $i++){
     ?>
     
     <div class="row">
         <div class="col-md-2">
-            <img src="img/2.jpg" class="img-thumbnail rounded">
+            <img src="<?php echo $result[$i]['image'];?>" class="img-thumbnail rounded">
         </div>
         <div class="col-md-9">
-            <h3>This is a new article</h3>
-            <p> In this video tutorial we are going to learn how to make a blog from scratch.<p>
-            <p><address>Pundrik</address>
+            <h3><?php echo $result[$i]['title'];?></h3>
+            <p> <?php echo $result[$i]['s_desc'];?><p>
+            <p><address><?php echo $result[$i]['auth'];?></address>
         </div>    
     </div>
     <hr>
